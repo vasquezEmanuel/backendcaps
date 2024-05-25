7a4dcb42f2ccadddd85cbb639418cb371d427344
vasquezEmanuel <emanuel.vasquez@udea.edu.co>
2024-05-25 17:49:37 -0500

    Creacion backend

---
+++ src/main/java/com/example/backendcaps/BackendcapsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendcaps/BackendcapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendcaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendcapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
