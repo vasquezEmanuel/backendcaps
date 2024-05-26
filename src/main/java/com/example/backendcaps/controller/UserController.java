1ead62a78cf555faf8c597706f15c0e7af39996a
vasquezEmanuel <emanuel.vasquez@udea.edu.co>
2024-05-25 22:35:02 -0500

    Creacón API user basica

--- src/main/java/com/example/backendcaps/controller/UserController.java
+++ src/main/java/com/example/backendcaps/controller/UserController.java
@@ -1,4 +1,37 @@
 package com.example.backendcaps.controller;
 
+import com.example.backendcaps.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.backendcaps.entity.User;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAll(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{UserId}")
+    public Optional<User> getById(@PathVariable("UserId") Long UserId){
+        return userService.getUser(UserId);
+    }
+
+
+    @DeleteMapping("/{UserId}")
+    public void delete(@PathVariable("UserId") Long UserId){
+        userService.delete(UserId);
+    }
+
+    @PostMapping
+    public void saveUpdate(@RequestBody User user){
+        userService.saveOrUpdate(user);
+    }
 }
